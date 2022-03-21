56bd09fe3c85251c46532dba5fcec5fb03951c36
Hao <927339269@qq.com>
2022-03-21 19:40:49 +0800

    update run.sh

--- tasks/pmf/run.sh
+++ tasks/pmf/run.sh
@@ -4,4 +4,7 @@
 python -m torch.distributed.launch --nproc_per_node=8 --master_port=63455 --use_env main.py config_server_nus.yaml
 
 ## my reproduce
-python -m torch.distributed.launch --nproc_per_node=1 --master_port=63445 --use_env main.py reproduce_kitti_0.yaml
+# 多卡
+python -m torch.distributed.launch --nproc_per_node=1 --master_port=63445 --use_env main.py --config_path reproduce_kitti_0.yaml
+# 单卡
+python main.py --config_path reproduce_kitti_0.yaml
